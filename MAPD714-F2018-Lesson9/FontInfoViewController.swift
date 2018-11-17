f6a4ab476593710051c0f315d27fbba4bf61f01a
tirthrajchauhan <tirthraj456@gmail.com>
2018-11-16 22:19:14 -0500

    Final Commit

--- MAPD714-F2018-Lesson9/FontInfoViewController.swift
+++ MAPD714-F2018-Lesson9/FontInfoViewController.swift
@@ -26,14 +26,14 @@ class FontInfoViewController: UIViewController {
         fontSampleLabel.text = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVv"
             + "WwXxYyZz 0123456789"
         fontSizeSlider.value = Float(font.pointSize)
-        fontSampleLabel.text = "\(Int(font.pointSize))"
+        fontSizeLabel.text = "\(Int(font.pointSize))"
         favoriteSwitch.isOn = favorite
     }
     
     @IBAction func slideFontSize(slider: UISlider){
         let newSize = roundf(slider.value)
         fontSampleLabel.font = font.withSize(CGFloat(newSize))
-        fontSampleLabel.text = "\(Int(newSize))"
+        fontSizeLabel.text = "\(Int(newSize))"
     }
     
     @IBAction func toggleFavorite(sender: UISwitch){
